c43fb5da554e64851a86d724e331f39c3bbd26ce
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-15 14:32:00 +0100

    Added a register page

---
+++ register.php
@@ -0,0 +1,5 @@
+<?php $title = 'Register Page'; require __DIR__ . "/include/header.php"; ?>
+     
+<?php require __DIR__ . "/components/registerForm.php"; ?>
+
+<?php require __DIR__ . "/include/footer.php"; ?>
