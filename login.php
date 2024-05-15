01045f9152691b5f8a2719c1c3ba60f8aa5037a2
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-15 15:09:59 +0100

    Added a login page for users

---
+++ login.php
@@ -0,0 +1,5 @@
+<?php $title = 'Login Page'; require __DIR__ . "/include/header.php"; ?>
+     
+<?php require __DIR__ . "/components/loginForm.php"; ?>
+
+<?php require __DIR__ . "/include/footer.php"; ?>
