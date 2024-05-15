79450ee5d9e8090b1da14c68186c339c44529b02
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-15 14:30:36 +0100

    Added code to register form

---
+++ components/registerForm.php
@@ -0,0 +1,107 @@
+<?php
+  
+  require_once './include/functions.php';
+
+  $message = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : '';
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST')
+  {
+    $fname = InputProcessor::processString($_POST['fname']);
+    $lname =  InputProcessor::processString($_POST['lname']);
+    $uname =  InputProcessor::processString($_POST['uname']);
+    $email =  InputProcessor::processEmail($_POST['email']);
+    $password =  InputProcessor::processPassword($_POST['password'], $_POST['password-v']);
+    $phone =  InputProcessor::processString($_POST['phone']);
+    $address =  InputProcessor::processString($_POST['address']);
+    
+    $valid = $fname['valid'] && $lname['valid'] && $uname['valid'] && $email['valid'] && $password['valid'] && $phone['valid'] && $address['valid'];
+
+    $message = !$valid ? "Please fix the above errors:" : '';
+
+    if ($valid)
+    {
+
+      $args = ['FirstName' => $fname['value'],
+               'LastName' => $lname['value'],
+               'UserName' => $uname['value'],
+               'Email' => $email['value'],
+               'HashedPassword' => password_hash($password['value'], PASSWORD_DEFAULT),
+               'Phone' => $phone['value'],
+               'Address' => $address['value']];
+
+      $user = $controllers->users()->register_user($args);
+      if ($user) {
+        redirect("login", ["error" => "Please login with your new account"]);
+      } else {
+        $message = "Email already registered.";
+      }
+      
+    }
+
+  }
+?>
+<form method="post" action=" <?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+  <section class="vh-100">
+    <div class="container py-5 h-75">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+
+              <h3 class="mb-2">Register</h3>
+              <div class="form-outline mb-4">
+                <input required type="text" id="fname" name="fname" class="form-control form-control-lg" placeholder="Firstname" value="<?= htmlspecialchars($fname['value'] ?? '') ?>"/>
+                <small class="text-danger"><?= htmlspecialchars($fname['error'] ?? '') ?></small>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input required type="text" id="lname" name="lname" class="form-control form-control-lg" placeholder="Lastname" value="<?= htmlspecialchars($lname['value'] ?? '') ?>"/>
+                <small class="text-danger"><?= htmlspecialchars($lname['error'] ?? '') ?></small>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input required type="text" id="uname" name="uname" class="form-control form-control-lg" placeholder="Username" value="<?= htmlspecialchars($uname['value'] ?? '') ?>"/>
+                <small class="text-danger"><?= htmlspecialchars($uname['error'] ?? '') ?></small>
+              </div>
+
+
+              <div class="form-outline mb-4">
+                <input required type="email" id="email" name="email" class="form-control form-control-lg" placeholder="Email" value="<?= htmlspecialchars($email['value']?? '') ?>" />
+                <small class="text-danger"><?= htmlspecialchars($email['error'] ?? '') ?></small>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input required type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Password" />
+              </div>
+              
+              <div class="form-outline mb-4">
+                <input required type="password" id="password-v" name="password-v" class="form-control form-control-lg" placeholder="Password again" />
+                <small class="text-danger"><?= htmlspecialchars($password['error'] ?? '') ?></small>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input required type="text" id="phone" name="phone" class="form-control form-control-lg" placeholder="Phone Number" value="<?= htmlspecialchars($uname['phone'] ?? '') ?>"/>
+                <small class="text-danger"><?= htmlspecialchars($phone['error'] ?? '') ?></small>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input required type="text" id="address" name="address" class="form-control form-control-lg" placeholder="Address" value="<?= htmlspecialchars($address['value'] ?? '') ?>"/>
+                <small class="text-danger"><?= htmlspecialchars($address['error'] ?? '') ?></small>
+              </div>
+
+              <button class="btn btn-primary btn-lg w-100 mb-4" type="submit">Register Account</button>
+              <a class="btn btn-secondary btn-lg w-100" type="submit" href="./login.php" >Already got an account?</a>
+
+              <?php if ($message): ?>
+                <div class="alert alert-danger mt-4">
+                  <?= $message ?? '' ?>
+                </div>
+              <?php endif ?>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</form>
