2133759b544ace36c411d0d77326a5697fe6f159
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-27 09:42:07 +0100

    Logout function

--- include/header.php
+++ include/header.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 $isAdmin = isset($_SESSION['user']) && $_SESSION['user']['Is_Admin'];
+$isLoggedIn = isset($_SESSION['user']);
+
+if (isset($_GET['logout'])) {
+    session_unset();
+    session_destroy();
+    header("Location: ./index.php");
+    exit();
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,8 +24,10 @@ $isAdmin = isset($_SESSION['user']) && $_SESSION['user']['Is_Admin'];
   <nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="./index.php">Broadleigh Gardens</a>
-    <a class="navbar-brand" href="./register.php">Register</a>
-    <a class="navbar-brand" href="./login.php">Login</a>
+    <?php if (!$isLoggedIn): ?>
+      <a class="navbar-brand" href="./register.php">Register</a>
+      <a class="navbar-brand" href="./login.php">Login</a>
+    <?php endif; ?>
     <a class="navbar-brand" href="./product.php">Shop</a> 
     <?php if ($isAdmin): ?>
       <a class="navbar-brand" href="./admin.php">Admin</a> 
@@ -28,9 +38,14 @@ $isAdmin = isset($_SESSION['user']) && $_SESSION['user']['Is_Admin'];
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="./user.php"><i class="bi bi-person-circle" style="font-size: 2rem"></i></a>
-        </li>
+        <?php if ($isLoggedIn): ?>
+          <li class="nav-item">
+            <a class="nav-link" href="./user.php"><i class="bi bi-person-circle" style="font-size: 2rem"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?logout=true">Logout</a>
+          </li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
