dbfd58e2f4849eaf0c8c4c5ee4dc753c901803d0
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-27 09:38:48 +0100

    Admin authentication for navbar

--- include/header.php
+++ include/header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$isAdmin = isset($_SESSION['user']) && $_SESSION['user']['Is_Admin'];
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -15,20 +19,22 @@
     <a class="navbar-brand" href="./register.php">Register</a>
     <a class="navbar-brand" href="./login.php">Login</a>
     <a class="navbar-brand" href="./product.php">Shop</a> 
-    <a class="navbar-brand" href="./admin.php">Admin</a> 
-    <a class="navbar-brand" href="./addProduct.php">Add Product</a> 
+    <?php if ($isAdmin): ?>
+      <a class="navbar-brand" href="./admin.php">Admin</a> 
+      <a class="navbar-brand" href="./addProduct.php">Add Product</a> 
+    <?php endif; ?>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-      <li class="nav-item">
+        <li class="nav-item">
           <a class="nav-link" href="./user.php"><i class="bi bi-person-circle" style="font-size: 2rem"></i></a>
-      </li>
+        </li>
       </ul>
-
-      
     </div>
   </div>
-</nav>
+  </nav>
+
+  </body>
+</html>
