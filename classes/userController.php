32368acdfc870e3777db14a9c470e52e8245521b
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-27 09:09:09 +0100

    updated login function

--- classes/userController.php
+++ classes/userController.php
@@ -61,15 +61,17 @@ class UserController {
     }   
 
     public function login_user(string $email, string $password)
-    {
-        $user = $this->get_user_by_email($email);
+{
+    $user = $this->get_user_by_email($email);
 
-        if ($user) {
-            $auth = password_verify($password,  $user['HashedPassword']);
-            return $auth ? $user : false;
+    if ($user) {
+        $auth = password_verify($password, $user['HashedPassword']);
+        if ($auth) {
+            return $user;
         }
-        return false;
     }
+    return false;
+}
 
 
 }
