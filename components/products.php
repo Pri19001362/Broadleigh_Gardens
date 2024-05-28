00cbf80172a18d4c38bc0f1d083564621842ccd6
Samuel Price <PRI19001362@btc.ac.uk>
2024-05-28 10:29:19 +0100

    updating styling

--- components/products.php
+++ components/products.php
@@ -15,40 +15,20 @@ $products = $controllers->products()->get_all_products($search_query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products</title>
 </head>
-<body>
+<body class="bg-light">
     <!-- Search form -->
-    <form action="" method="GET">
-        <input type="text" name="search" placeholder="Search Products" value="<?= htmlspecialchars($search_query ?? '') ?>">
-        <button type="submit">Search</button>
+    <form action="" method="GET" class="p-3">
+        <input type="text" name="search" placeholder="Search Products" value="<?= htmlspecialchars($search_query ?? '') ?>" class="form-control mb-2">
+        <button type="submit" class="btn btn-primary">Search</button>
     </form>
 
     <!-- Display products -->
     <?php if (empty($products)): ?>
-        <p>No products found.</p>
+        <p class="text-center">No products found.</p>
     <?php else: ?>
-        <div class="row">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
             <?php foreach ($products as $product): ?>
-                <div class="col-4">
-                    <div class="card">
-                        <img src="<?= $product['Image'] ?>" class="card-img-top" alt="image of <?= $product['Description'] ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $product['Name'] ?></h5>
-                            <p class="card-text"><?= $product['Description'] ?></p>
-                            <p class="card-text"><?= $product['Category'] ?></p>
-                            <p class="card-text"><?= $product['Price'] ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
-
-    <!-- Display all products if no products are found -->
-    <?php if (empty($products)): ?>
-        <p>Showing all products:</p>
-        <div class="row">
-            <?php foreach ($controllers->products()->get_all_products() as $product): ?>
-                <div class="col-4">
+                <div class="col">
                     <div class="card">
                         <img src="<?= $product['Image'] ?>" class="card-img-top" alt="image of <?= $product['Description'] ?>">
                         <div class="card-body">
